e3829127d6fd10983e909642733a8086dc6adb6d
Ravikumar <96512246+ravikumar0112@users.noreply.github.com>
2025-08-22 19:46:17 +0530

    Update Lab prog 3.py
    This program demonstrates basic data handling using pandas, it shows how to create, explore, modify, and combine data using pandas.

--- Lab prog 3.py
+++ Lab prog 3.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep  5 21:06:35 2021
 
-@author: saras
-"""
-'''
 import pandas as pd
 import numpy as np
 data = np.array([1,2,3,4,5,6,7,8,87,90,23,45])
@@ -17,7 +11,7 @@ print (s[[0,1]])
 print (s.values)
 print (s.index)
 
-'''
+
 
 import pandas as pd
 data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42],'Dept': ['AI','CSE','AI','CSE']}
@@ -29,11 +23,11 @@ data1 = pd.DataFrame({'Name':['Mercy','Jackey'],'Age':[34,56],'Dept':['AI','CSE'
 #print (df['Age'])
 #df['dept']= pd.Series(['AI','CSE','AI','CSE'],index=['rank1','rank2','rank3','rank4'])
 #print (df)
-'''
+
 print ("Deleting the first column using DEL function:")
 del df['Name']
 print(df)
-'''
+
 #print(df.iloc[0])
 #print (df[2:4])
 
@@ -41,4 +35,5 @@ print(data1)
 df = df.append(data1)
 df = df.drop('rank1')
 print(df)
+
 
